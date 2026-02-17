4b4b936116f99a3b50576098f9971fec32636433
Alexssander Fonseca <alex.marques.fa@gmail.com>
2026-02-17 17:51:16 -0300

    feat: setup

--- rds-orcamento/terraform/backend.tf
+++ rds-orcamento/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "tfstate-fiap-alex-academy-rds"
+    bucket  = "tfstate-fiap-alex-academy-rds-1"
     key     = "rds-orcamento/terraform.tfstate"
     region  = "us-east-1"
     encrypt = false
