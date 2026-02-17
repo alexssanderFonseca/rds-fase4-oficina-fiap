ee781aa16c31390dd1e90f9744ebc96633fc2b22
Alexssander Fonseca <alex.marques.fa@gmail.com>
2026-02-17 18:20:18 -0300

    feat: ajustes

--- rds-cliente/terraform/data.tf
+++ rds-cliente/terraform/data.tf
@@ -14,7 +14,3 @@ data "aws_subnet" "details" {
   for_each = toset(data.aws_subnets.all.ids)
   id       = each.value
 }
-
-data "aws_secretsmanager_secret" "rds_credentials" {
-  arn = "arn:aws:secretsmanager:us-east-1:305448253775:secret:secrets-XmZ0Fb"
-}
